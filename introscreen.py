1d50d5a7ac7e61ccb0d0406c2a31eb3d29590ce7
fayaznaim <fayaznaim@gmail.com>
2020-04-20 21:29:33 -0500

    intriscreen text update

--- introscreen.py
+++ introscreen.py
@@ -2,7 +2,7 @@
 
 
 def introscreen():
-    print("         *** URL ANALYSIS SCRIPT FOR SCI CYBER ANALYSTS ***\n")
+    print("         *** URL ANALYSIS SCRIPT FOR CYBER ANALYSTS ***\n")
     print("         This program provides analysis information via the following websites.\n"
           "         [ URLVoid ][ Norton Safeweb ][ TALOSIntelligence ][ ThreatMiner ][ AbuseIPDB ]\n"
           "         Also displays the scraped desired text in the output.\n\n")
